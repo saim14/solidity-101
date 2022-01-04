35eea17ec07b97db55909b2b88c147e54245e1db
saim14 <saim.islam698@gmail.com>
2022-01-04 11:32:05 +0600

    Factory Storage, Inheritence and use Contract and deploy from another one

---
+++ 01 - StorageFactory1.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
+import "./00 - SimpleStorage.sol";
+
+contract StorageFactory {
+    SimpleStorage[] public simpleStorageArray;
+    function createSimpleStorageContact() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public{
+        // Address, ABI 
+        SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageNumber) public view returns(uint256){
+       return SimpleStorage(address(simpleStorageArray[_simpleStorageNumber])).get();
+        
+    }
+}
